3f53a2459b451a6be1767a51135575cbff02c253
CherutoMercy <m.thirty100@gmail.com>
2017-01-29 09:25:59 +0300

    add server file linking all files

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var engine = require('ejs-mate');
+var ejs = require('ejs');
+var flash = require('connect-flash');
+
+// set static folder
+app.use(express.static(__dirname + '/assets'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/uploads', express.static('uploads'));
+
+
+//Create EJS Engine view
+app.set('view engine', 'html');
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.use(flash());
+
+
+var routes = require('./routes/index.js');
+app.use(routes);
+
+app.set('port', (process.env.PORT || 8000));
+app.listen(app.get('port'), function() {
+  console.log('Hurray am running on port ' + app.get('port'))
+});
