fa0176b9b76f7bdfa0e563a87a40354385aad641
CherutoMercy <m.thirty100@gmail.com>
2017-02-04 17:51:22 +0300

    some styling

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@ var router         = express.Router();
 var homeRoutes     = require('./home');
 var landRoutes     = require('./land');
 var userRoutes     = require('./user');
+var designersRoutes     = require('./designers');
+var modelsRoutes     = require('./models');
+var photographyRoutes     = require('./photography');
+var photoshootsRoutes     = require('./photoshoots');
+
 
 router.get('/',        homeRoutes.index);
 router.get('/landing', landRoutes.index);
+router.get('/designers', designersRoutes.index);
+router.get('/models', modelsRoutes .index);
+router.get('/photography', photographyRoutes.index);
+router.get('/photoshoots', photoshootsRoutes.index);
 router.get('/profile', userRoutes.index);
 
 module.exports=router;
